56e8cae6980f0cf319c0b4afd25fb39dbb58e8ad
Sara <Sara@10.157.6.107>
2020-02-05 17:38:58 +0100

    first commit

---
+++ src/projetjava/ProjetJava.java
@@ -0,0 +1,17 @@
+
+package projetjava;
+
+/**
+ *
+ * @author Sara
+ */
+public class ProjetJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
